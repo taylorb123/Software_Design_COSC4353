9fb431abc509e0cee1ce0c531e716d1ca6070e7b
taylorb123 <taylorbarnett281@yahoo.com>
2022-02-20 16:46:26 -0600

    added update qote and delete quote functionality in api

--- backend/controllers/fuel-controllers.js
+++ backend/controllers/fuel-controllers.js
@@ -1,10 +1,10 @@
 const HttpError = require('../models/http-error');
 
-const DUMMY_QUOTE = [
+let DUMMY_QUOTE = [
     {
         gallons: '2',
         address1: '123 fake street',
-        Address2: '456 evenFaker street',
+        address2: '456 evenFaker street',
         date: '02/20/2022',
         ppg: '2.00',
         total: '500',
@@ -12,35 +12,63 @@ const DUMMY_QUOTE = [
     }
 ];
 
-const getQuoteByUsername = (req,res,next) => {        //return json formatted DUMMY_QUOTE by username
+const getQuotesByUsername = (req,res,next) => {        //return json formatted DUMMY_QUOTE by username
     const quoteByUser = req.params.username;
-    const quote = DUMMY_QUOTE.find(p => {
+    const quotes = DUMMY_QUOTE.filter(p => {
         return p.username === quoteByUser;
     });
 
-    if(!quote){
-        return next(new HttpError('Could not find quote for given username', 404));
+    if(!quotes || quotes.length === 0){
+        return next(new HttpError('Could not find quotes for given username', 404));
     }
 
     console.log('GET Request in fuelquotes');
-    res.json({quote});
+    res.json({quotes});
 };
 
 const createQuote = (req,res,next) => {
-    const {gallons,address1,Address2,date,ppg,total,username } = req.body;
+    const {gallons,address1,address2,date,ppg,total,username } = req.body;
 
     const createQuote = {
         gallons,
         address1,
-        Address2,
+        address2,
         date,
         ppg,
         total,
         username
     };
     DUMMY_QUOTE.push(createQuote);
-    res.status(201).json({place: createQuote});
+    res.status(201).json({quote: createQuote});
 };
 
-exports.getQuoteByUsername = getQuoteByUsername;
+const updateQuote = (req,res,next) => {
+    const {gallons,address1,address2,date,ppg,total} = req.body;
+    const userQuote = req.params.username;
+
+    const updateQuote ={ ...DUMMY_QUOTE.find(p => p.username === userQuote) };
+    const formIndex = DUMMY_QUOTE.findIndex(p => p.username == userQuote);
+    updateQuote.gallons = gallons;
+    updateQuote.address1 = address1;
+    updateQuote.address2 = address2;
+    updateQuote.date = date;
+    updateQuote.ppg = ppg;
+    updateQuote.total = total;
+
+    DUMMY_QUOTE[formIndex] = updateQuote;
+    res.status(200).json({quote: updateQuote});
+};
+
+const deleteQuote = (req,res,next) => {
+    const userQuote = req.params.username;
+    DUMMY_QUOTE = DUMMY_QUOTE.filter(p => p.username !== userQuote);
+    res.status(200).json({message: 'Deleted Quote'});
+};
+
+
+
+
+exports.getQuotesByUsername = getQuotesByUsername;
 exports.createQuote = createQuote;
+exports.updateQuote = updateQuote;
+exports.deleteQuote = deleteQuote;
--- backend/routes/fuel-routes.js
+++ backend/routes/fuel-routes.js
@@ -4,7 +4,8 @@ const router            = express.Router();
 
 
 
-router.get('/:username', fuelControllers.getQuoteByUsername);
-router.post('/', fuelControllers.createQuote);
-
+router.get('/:username', fuelControllers.getQuotesByUsername);
+router.post('/', fuelControllers.createQuote);                      //create a new quote
+router.patch('/:username', fuelControllers.updateQuote);            //update an exsisting quote by username
+router.delete('/:username', fuelControllers.deleteQuote);           //detele an exsisting quote by username
 module.exports = router;
