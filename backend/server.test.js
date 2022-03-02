ebe46275e3307f08c77b2975b521beee5e525cba
taylorb123 <taylorbarnett281@yahoo.com>
2022-03-01 18:57:28 -0600

    added additional jest testcases

--- backend/server.test.js
+++ backend/server.test.js
@@ -6,7 +6,7 @@ const request = require('supertest');
 //server.listen(5678, () => {
 describe("POST /login", () =>{
 
-    describe("given a username and password", () =>{
+    describe("Given a username and password is correct", () =>{
         //should save the username and password to localhost
         //should respond with json object containing the username and password
         test("should respond with a 200 status cade", async () =>{
@@ -16,10 +16,55 @@ describe("POST /login", () =>{
             })
             expect (response.statusCode).toBe(200)
         })
+    }),
+    describe("Given a username and password is incorrect", () =>{
+        test("should respond with a 401 status cade", async () =>{
+            const response = await request(server).post("/api/users/login").send({
+                "username": "taylor123",
+                "password": "password"
+            })
+            expect (response.statusCode).toBe(401)
+        })
     })
 
-    describe('When the username or password is missing', () => { 
-        //Could not find user credentials', 401
+})
+
+// describe("POST /login", () =>{
+
+//     describe("Given a username and password is incorrect", () =>{
+//         test("should respond with a 401 status cade", async () =>{
+//             const response = await request(server).post("/api/users/login").send({
+//                 "username": "taylor123",
+//                 "password": "password"
+//             })
+//             expect (response.statusCode).toBe(401)
+//         })
+//     })
+// })
+
+
+
+describe("GET /", () =>{
+    describe("Retrieve a list of stored users", () =>{
+        test("should respond with a 401 status cade", async () =>{
+            const response = await request(server).get("/api/users")
+            expect (response.statusCode).toBe(200)
+        }),
+    describe("Retrieve a users account information by username", () =>{
+        test("status code response 200", async () =>{
+              const response = await request(server).get("/api/fuelquote/taylor")
+              expect(response.statusCode).toBe(200)
+        })
+    }),
+    describe("Retrieve user account information with incorrect credentials", () =>{
+        test("Status code response 404", async () =>{
+              const response = await request(server).get("/api/fuelquote/dummy_user")
+              expect(response.statusCode).toBe(404)
+        })
     })
 })
-//})
+
+})
+
+
+
